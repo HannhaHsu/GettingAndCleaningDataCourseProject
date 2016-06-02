b3b15d4390bc92a1656c06c8fd31d8b7d29ee66c
HannhaHsu <growtree1061@gmail.com>
2016-06-01 21:32:03 -0700

    Add files via upload

---
+++ run_analysis.R
@@ -0,0 +1,64 @@
+###Data Preparation
+#Download and unzip data
+if(!file.exists("./getting_cleaning_data")){dir.create("./getting_cleaning_data")}
+fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+download.file(fileUrl, destfile="./getting_cleaning_data/Dataset.zip", method="curl")
+unzip(zipfile="./getting_cleaning_data/Dataset.zip", exdir="./getting_cleaning_data")
+path_uci <- file.path("./getting_cleaning_data/", "UCI HAR Dataset")
+files <- list.files(path_uci, recursive = TRUE)
+files
+
+#Read data into the variables from the files and display the information of the variables
+activityTest <- read.table(file.path(path_uci, "test", "y_test.txt"), header = FALSE)
+activityTrain <- read.table(file.path(path_uci, "train", "y_train.txt"), header = FALSE)
+
+subjectTrain <- read.table(file.path(path_uci, "train", "subject_train.txt"), header = FALSE)
+subjectTest <- read.table(file.path(path_uci, "test", "subject_test.txt"), header = FALSE)
+
+featureTest <- read.table(file.path(path_uci, "test", "X_test.txt"), header = FALSE)
+featureTrain <- read.table(file.path(path_uci, "train", "X_train.txt"), header = FALSE)
+
+str(activityTest)
+str(activityTrain)
+
+str(subjectTest)
+str(subjectTrain)
+
+str(featureTest)
+str(featureTrain)
+
+###Merge columns to get the data frame for all data
+#Concatenate the training and test data tables by rows
+activityData <- rbind(activityTrain, activityTest)
+subjectData <- rbind(subjectTrain, subjectTest)
+featureData <- rbind(featureTrain, featureTest)
+
+###Appropriately labels the data set with descriptive variable names.
+names(subjectData) <- c("subject")
+names(activityData) <- c("activity")
+featureNames <- read.table(file.path(path_uci, "features.txt"), head = FALSE)
+names(featureData) <- featureNames$V2
+
+#Concatenate subject, activity and feature data by column
+dataCombine <- cbind(subjectData, activityData)
+Data <- cbind(featureData, dataCombine)
+
+###Extracts only the measurements on the mean and standard deviation for each measurement
+subdataFeatureNames <- featureNames$V2[grep("mean\\(\\)|std\\(\\)", featureNames$V2)]
+
+###Uses descriptive activity names to name the activities in the data set
+#Subset the data frame Data by selected names of Features
+selectedNames <- c(as.character(subdataFeatureNames), "subject", "activity")
+Data <- subset(Data, select=selectedNames)
+#Check
+str(Data)
+#Label activities
+activityLabels <- read.table(file.path(path_uci, "activity_labels.txt"), header = FALSE)
+head(Data$activity)
+names(Data)
+
+###Creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+library(plyr);
+Data2<-aggregate(. ~subject + activity, Data, mean)
+write.table(Data2, file = "tidydata.txt", row.name = FALSE)
+
