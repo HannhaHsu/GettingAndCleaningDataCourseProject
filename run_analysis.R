9fd4e2b6586764731bb92860496c2c7363bcee29
HannhaHsu <growtree1061@gmail.com>
2016-06-03 01:28:15 -0700

    Add files via upload

--- run_analysis.R
+++ run_analysis.R
@@ -1,67 +1,73 @@
-#Download and unzip data if data does not already exist in working directory
-if(!file.exists("./getting_cleaning_data")){dir.create("./getting_cleaning_data")}
+library(plyr);
+
+###Download and unzip data if data does not already exist in working directory###
 fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
-download.file(fileUrl, destfile="./getting_cleaning_data/Dataset.zip", method="curl")
+if(!file.exists("./getting_cleaning_data")){
+  dir.create("./getting_cleaning_data")
+  download.file(fileUrl, destfile="./getting_cleaning_data/Dataset.zip", method="curl")
+}
 unzip(zipfile="./getting_cleaning_data/Dataset.zip", exdir="./getting_cleaning_data")
-path_uci <- file.path("./getting_cleaning_data/", "UCI HAR Dataset")
-files <- list.files(path_uci, recursive = TRUE)
-#Check
-files
+ucifile_path <- file.path("./getting_cleaning_data/", "UCI HAR Dataset")
+files <- list.files(ucifile_path, recursive = TRUE)
+####Download and unzip data if data does not already exist in working directory###
 
-#Read data into the variables from the files and display the information of the variables
-activityTest <- read.table(file.path(path_uci, "test", "y_test.txt"), header = FALSE)
-activityTrain <- read.table(file.path(path_uci, "train", "y_train.txt"), header = FALSE)
-
-subjectTrain <- read.table(file.path(path_uci, "train", "subject_train.txt"), header = FALSE)
-subjectTest <- read.table(file.path(path_uci, "test", "subject_test.txt"), header = FALSE)
-
-featureTest <- read.table(file.path(path_uci, "test", "X_test.txt"), header = FALSE)
-featureTrain <- read.table(file.path(path_uci, "train", "X_train.txt"), header = FALSE)
-
-#Check
+###Read data into the variables from the files and display the information of the variables###
+activityTest <- read.table(file.path(ucifile_path, "test", "y_test.txt"), header = FALSE)
+activityTrain <- read.table(file.path(ucifile_path, "train", "y_train.txt"), header = FALSE)
+subjectTrain <- read.table(file.path(ucifile_path, "train", "subject_train.txt"), header = FALSE)
+subjectTest <- read.table(file.path(ucifile_path, "test", "subject_test.txt"), header = FALSE)
+featureTest <- read.table(file.path(ucifile_path, "test", "X_test.txt"), header = FALSE)
+featureTrain <- read.table(file.path(ucifile_path, "train", "X_train.txt"), header = FALSE)
 str(activityTest)
 str(activityTrain)
-
 str(subjectTest)
 str(subjectTrain)
-
 str(featureTest)
 str(featureTrain)
+###Read data into the variables from the files and display the information of the variables###
 
-#Concatenate the training and test data tables by rows
+###Concatenate the training and test data tables by rows###
 activityData <- rbind(activityTrain, activityTest)
 subjectData <- rbind(subjectTrain, subjectTest)
 featureData <- rbind(featureTrain, featureTest)
+###Concatenate the training and test data tables by rows###
 
-#Appropriately labels the data set with descriptive variable names.
+###Appropriately labels the data set with descriptive variable names###
 names(subjectData) <- c("subject")
 names(activityData) <- c("activity")
-featureNames <- read.table(file.path(path_uci, "features.txt"), head = FALSE)
+featureNames <- read.table(file.path(ucifile_path, "features.txt"), head = FALSE)
 names(featureData) <- featureNames$V2
+###Appropriately labels the data set with descriptive variable names###
 
-#Concatenate subject, activity and feature data by column
+###Concatenate subject, activity and feature data by column###
 dataCombine <- cbind(subjectData, activityData)
 Data <- cbind(featureData, dataCombine)
+###Concatenate subject, activity and feature data by column###
 
-#Extracts only the measurements on the mean and standard deviation for each measurement
+###Extracts only the measurements on the mean and standard deviation for each measurement###
 subdataFeatureNames <- featureNames$V2[grep("mean\\(\\)|std\\(\\)", featureNames$V2)]
+###Extracts only the measurements on the mean and standard deviation for each measurement###
 
-#Uses descriptive activity names to name the activities in the data set
+###Uses descriptive activity names to name the activities in the data set###
 selectedNames <- c(as.character(subdataFeatureNames), "subject", "activity")
+###Uses descriptive activity names to name the activities in the data set###
 
-#Subset the data frame Data by selected names of Features
+###Subset the data frame Data by selected names of Features###
 Data <- subset(Data, select=selectedNames)
-
-#Check
 str(Data)
+###Subset the data frame Data by selected names of Features###
 
-#Label activities
-activityLabels <- read.table(file.path(path_uci, "activity_labels.txt"), header = FALSE)
+
+###Label activities###
+activityLabels <- read.table(file.path(ucifile_path, "activity_labels.txt"), header = FALSE)
+Data$activity<-factor(Data$activity);
+Data$activity<- factor(Data$activity,labels=as.character(activityLabels$V2))
 head(Data$activity)
 names(Data)
+###Label activities###
 
-###Creates a second, independent tidy data set with the average of each variable for each activity and each subject.
-library(plyr);
+###Creates a second, independent tidy data set with the average of each variable for each activity and each subject###
 Data2<-aggregate(. ~subject + activity, Data, mean)
+Data2<-Data2[order(Data2$subject,Data2$activity),]
 write.table(Data2, file = "tidydata.txt", row.name = FALSE)
-
+###Creates a second, independent tidy data set with the average of each variable for each activity and each subject###
